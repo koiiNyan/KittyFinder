2eb08f1e81779d00f918d28e112552e1fe403c9d
Anastasia <anovinnikova@yandex.ru>
2022-06-07 16:57:56 +0300

    Fixed infinite subtraction by clicking on one kitty

--- Assets/Scripts/Kitty.cs
+++ Assets/Scripts/Kitty.cs
@@ -7,13 +7,16 @@ namespace KittyFinder
     {
         [SerializeField]
         private GameObject _circle;
+        [SerializeField]
+        private bool _isNotClicked = true;
 
         public event ClickEventHandler OnClickEventHandler;
         public void OnPointerClick(PointerEventData eventData)
         {
             Debug.Log("Clicked Kitty");
             _circle.SetActive(true);
-            OnClickEventHandler?.Invoke(this);
+            if (_isNotClicked) OnClickEventHandler?.Invoke(this);
+            _isNotClicked = false;
         }
 
         public delegate void ClickEventHandler(Kitty kittyComponent);
