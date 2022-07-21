7ffda92c195ea395d5cdfbe0621d9a302a7a0293
Anastasia <anovinnikova@yandex.ru>
2022-07-21 17:32:54 +0300

    Full game, new features and clean background

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,11 +13,58 @@ namespace KittyFinder
         private Text _kittiesNumberText;
         private int _kittiesLeft = 50;
         private const int c_originalKittiesLeft = 50;
+        [SerializeField]
+        private ParticleSystem _congrateParticle;
+        [SerializeField]
+        private float _particleSeconds = 8f;
+        [SerializeField]
+        private GameObject _victoryText;
+
+        private bool _isGameRunning = true;
+        private Coroutine coUpdateTimer;
+        [SerializeField] private Text _timer_Txt;
+        [SerializeField] private float _currentTime;
+        private string _timePlayingStr;
+        private TimeSpan _timePlaying;
+
+        private AudioSource _gameAudio;
+        [SerializeField] private AudioClip _victorySound;
+        [SerializeField] private AudioClip _backSound;
+
 
         private void Awake()
         {
             FollowEvents();
             LoadKitties();
+            BeginTimer();
+            _gameAudio = GetComponent<AudioSource>();
+        }
+
+        private void BeginTimer()
+        {
+
+            if (coUpdateTimer != null)
+            {
+                StopCoroutine(coUpdateTimer);
+            }
+
+            coUpdateTimer = StartCoroutine(UpdateTimer());
+        }
+
+        private IEnumerator UpdateTimer()
+        {
+            while (_isGameRunning)
+            {
+                _currentTime += Time.deltaTime;
+
+                _timePlaying = TimeSpan.FromSeconds(_currentTime);
+                _timePlayingStr = _timePlaying.ToString("mm':'ss");
+               // Debug.Log(_timePlaying);
+                Debug.Log(_timePlayingStr);
+                _timer_Txt.text = _timePlayingStr;
+
+                yield return null;
+            }
         }
 
         private void FollowEvents()
@@ -30,7 +79,13 @@ namespace KittyFinder
         {
             Debug.Log($"Kitty {kitty} Clicked");
             SetKittyNumber(_kittiesLeft-1);
-            Debug.Log(_kittiesLeft - 1);
+            Debug.Log(_kittiesLeft);
+
+            //_kittiesLeft = 0;
+            if (_kittiesLeft == 0)
+            {
+                GameWin();
+            }
         }
 
         private void SetKittyNumber(int kittyNumber)
@@ -38,6 +93,26 @@ namespace KittyFinder
             _kittiesLeft = kittyNumber;
             _kittiesNumberText.text = _kittiesLeft.ToString();
         }
+
+        private void GameWin()
+        {
+            Debug.Log("GameWin");
+            _isGameRunning = false;
+            _gameAudio.Stop();
+            _gameAudio.PlayOneShot(_victorySound);
+            StartCoroutine(PlayParticles(_particleSeconds));
+        }
+
+        private IEnumerator PlayParticles(float secs)
+        {
+            Debug.Log("PlayParticles");
+            _congrateParticle.Play();
+            _victoryText.SetActive(true);
+            yield return new WaitForSeconds(secs);
+            _congrateParticle.Stop();
+            _victoryText.SetActive(false);
+        }
+
 
         public void ExitGameButton()
         {
@@ -60,6 +135,13 @@ namespace KittyFinder
             {
                 kitty.SetKittyActivity(true);
             }
+            StopAllCoroutines();
+            _currentTime = 0;
+
+            _isGameRunning = true;
+            BeginTimer();
+            _gameAudio.Play();//////////////////////////
+
         }
 
         private void OnApplicationPause(bool pause)
--- Assets/Scripts/Kitty.cs
+++ Assets/Scripts/Kitty.cs
@@ -7,14 +7,23 @@ namespace KittyFinder
     {
         [SerializeField]
         private GameObject _circle;
+        private AudioSource _kittyAudio;
+        [SerializeField] private AudioClip _kittySound;
 
         public bool IsNotClicked { get; private set; } = true;
+
+        private void Start()
+        {
+            _kittyAudio = GetComponent<AudioSource>();
+        }
 
         public event ClickEventHandler OnClickEventHandler;
         public void OnPointerClick(PointerEventData eventData)
         {
             Debug.Log("Clicked Kitty");
             _circle.SetActive(true);
+            _kittyAudio.PlayOneShot(_kittySound);
+
             if (IsNotClicked) OnClickEventHandler?.Invoke(this);
             IsNotClicked = false;
         }
